e1e0c20bd8c15bb5770c1bba5a1c35af6fe9d34f
Chico Charlesworth <chico.charlesworth@googlemail.com>
2012-09-29 14:54:41 +0100

    Added initial bootstrap html5 layout

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,21 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
 import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
   
   public static Result index() {
     return ok(index.render());
+  }
+  
+  public static Result reports() {
+    return ok(reports.render());
+  }
+  
+  public static Result feedback() {
+    return ok(feedback.render());
   }
   
 }
